e7e2243205247751c3af2d0f9c57cc964a053c92
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2016-06-21 16:05:16 +0300

    add long-running test

---
+++ AjaxControlToolkit.Tests/LongRunningTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AjaxControlToolkit.Tests {
+    [TestFixture]
+    public class LongRunningTests {
+
+        [Test]
+        public void TestThatRuns5Minutes() {
+            Thread.Sleep(TimeSpan.FromMinutes(5));
+
+            Assert.True(true);
+        }
+    }
+}
